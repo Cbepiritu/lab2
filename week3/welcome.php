59e505c4d16aaceb08a4e9874612dc704033a2e7
Carl Jethro Espiritu <cbespiritu@student.apc.edu.ph>
2023-06-02 13:47:36 +0800

    para sa laptop ko sa bahay

---
+++ week3/welcome.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+Welcome <?php echo $_POST["n"]; ?><br>
+Your email address is: <?php echo $_POST["mail"]; ?>
+
+</body>
+</html>
